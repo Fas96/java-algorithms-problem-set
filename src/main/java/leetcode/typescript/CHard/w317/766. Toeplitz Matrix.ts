331a6433d1867d113594dc1d1ff363cb00bf75ad
fasbhim <https://gitlab.argonet.co.kr/Fas96>
2022-11-03 22:51:32 +0900

    "algo"

--- src/main/java/leetcode/typescript/CHard/w317/766. Toeplitz Matrix.ts
+++ src/main/java/leetcode/typescript/CHard/w317/766. Toeplitz Matrix.ts
@@ -9,3 +9,32 @@ function isToeplitzMatrix(matrix: number[][]): boolean {
     return true;
 
 };
+{
+    "isBundle": null,
+    "code": "N00000001",
+    "endDate": "2022-12-01T00:00:00.000Z",
+    "creatorId": "admin",
+    "isContainer": true,
+    "projYear": "2019",
+    "title": "임베디드 음성인식 AI 기능을 탑재한 차량 오버 헤드 콘솔 제어 모듈 개발",
+    "noteStatus": null,
+    "noteNo": null,
+    "dcsource": null,
+    "modified": "2022-11-02T02:31:36.434Z",
+    "noteKind": null,
+    "authPermission": null,
+    "fileModified": null,
+    "creator": "Administrator",
+    "created": "2020-07-03T04:14:46.349Z",
+    "projStatus": "open",
+    "userName": "user01",
+    "sharedDate": null,
+    "firstName": "김구",
+    "isFolder": false,
+    "size": null,
+    "nodeRef": "workspace://SpacesStore/ad9799ed-fea3-49ed-a003-3afc75f86d31",
+    "fileCreated": null,
+    "name": "000000001",
+    "isShared": false,
+    "startDate": "2019-01-01T00:00:00.000Z"
+}
